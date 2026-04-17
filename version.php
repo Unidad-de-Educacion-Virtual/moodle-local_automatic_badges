229a1a4612c3461ead6bf11aae4d8c258b5ad13b
JulianL76 <jul_lamus76@outlook.com>
2026-04-17 00:50:10 -0500

    Version fix

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_automatic_badges';
-$plugin->version = 2026033100;
+$plugin->version = 2026041701;
 $plugin->requires = 2024100700; // Moodle 4.5+.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.6';
+$plugin->release = '1.1.1';
